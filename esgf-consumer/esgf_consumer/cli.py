b9b9f781d6dae90c55c319340dfb5d6911b4f1a4
david.poulter <david.poulter@stfc.ac.uk>
2024-07-22 16:49:10 +0100

    Fix typo in import

--- esgf-consumer/esgf_consumer/cli.py
+++ esgf-consumer/esgf_consumer/cli.py
@@ -2,7 +2,7 @@ import asyncio
 import logging
 
 import click
-from esgf_playgroud_utils.config.kafka import Settings
+from esgf_playground_utils.config.kafka import Settings
 
 from esgf_consumer import consume
 
