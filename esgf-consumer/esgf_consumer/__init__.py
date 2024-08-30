aa8a9b259a26d038ccfda38beb4322a0b106f302
Edbo849 <edborthwick84@gmail.com>
2024-08-30 15:59:20 +0100

    Change

--- esgf-consumer/esgf_consumer/__init__.py
+++ esgf-consumer/esgf_consumer/__init__.py
@@ -10,21 +10,14 @@ import httpx
 from aiokafka.errors import KafkaError
 from esgf_consumer.collection import ensure_collection
 from esgf_consumer.consumers import get_consumer
-from esgf_consumer.exceptions import (
-    ESGFConsumerNotImplementedPayloadError,
-    ESGFConsumerUnknownPayloadError,
-)
+from esgf_consumer.exceptions import (ESGFConsumerNotImplementedPayloadError,
+                                      ESGFConsumerUnknownPayloadError)
 from esgf_consumer.items import create_item, hard_delete_item, update_item
 from esgf_consumer.producers import get_producer
 from esgf_playground_utils.config.kafka import Settings
-from esgf_playground_utils.models.kafka import (
-    CreatePayload,
-    Error,
-    ErrorType,
-    KafkaEvent,
-    RevokePayload,
-    UpdatePayload,
-)
+from esgf_playground_utils.models.kafka import (CreatePayload, Error,
+                                                ErrorType, KafkaEvent,
+                                                RevokePayload, UpdatePayload)
 from pydantic import ValidationError
 
 logging.getLogger().setLevel(logging.DEBUG)
