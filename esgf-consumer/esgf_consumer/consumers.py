5415db977ab64530805553cd5802b9148126ca42
rhysrevans3 <rhys.r.evans3@gmail.com>
2025-01-22 10:22:35 +0000

    Adding ssl_context.

--- esgf-consumer/esgf_consumer/consumers.py
+++ esgf-consumer/esgf_consumer/consumers.py
@@ -1,8 +1,11 @@
-from aiokafka import AIOKafkaConsumer
+from aiokafka import AIOKafkaConsumer, helpers
 from esgf_playground_utils.config.kafka import Settings
 
 
 async def get_consumer(settings: Settings) -> AIOKafkaConsumer:
+    ssl_context = (
+        helpers.create_ssl_context() if settings.sasl_mechanism == "SASL_SSL" else None
+    )
     consumer = AIOKafkaConsumer(
         group_id=settings.consumer_group,
         bootstrap_servers=settings.bootstrap_servers,
@@ -10,6 +13,7 @@ async def get_consumer(settings: Settings) -> AIOKafkaConsumer:
         security_protocol=settings.security_protocol,
         sasl_plain_username=settings.sasl_plain_username,
         sasl_plain_password=settings.sasl_plain_password,
+        ssl_context=ssl_context,
         enable_auto_commit=True,
         auto_commit_interval_ms=5000,
         auto_offset_reset="earliest",
